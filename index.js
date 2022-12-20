f022153518e291bc27ce4e59f5db9ecdd8e10ed7
João Vitor Bruno Cunha Santos <Joaovitorbrunocunhasantos@gmail.com>
2022-12-19 21:49:12 -0300

    atualizaçoes

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 function redirecao(){
     window.location = "/home/home.html"
 };
-function voltar(){
-    window.location = "/estilos/index.html"
-}
-let ul = document.querySelector('li')
-function meuShow(){
-    ul.addEventListener("click", e=>{
-        ul.classList.remove
-    })
-}
+
+
 
     
