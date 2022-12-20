c1cb1f70de8f455fc8557e19c7c166dd523a0417
João Vitor Bruno Cunha Santos <Joaovitorbrunocunhasantos@gmail.com>
2022-12-19 21:43:42 -0300

    melhorias

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function redirecao(){
 function voltar(){
     window.location = "/estilos/index.html"
 }
-
+let ul = document.querySelector('li')
+function meuShow(){
+    ul.addEventListener("click", e=>{
+        ul.classList.remove
+    })
+}
 
     
